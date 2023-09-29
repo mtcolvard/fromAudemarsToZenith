7f2e85333c3988e80374f440a2cf86a46783804b
Matty Talty <matthew.colvard@gmail.com>
2023-09-28 20:26:49 -0700

    landing page photo resize

--- app/page.js
+++ app/page.js
@@ -7,17 +7,18 @@ export default function Home() {
     <main className="bg-black relative min-h-screen">
       <div className="absolute inset-0 flex items-center justify-center z-10">
         <Link href="/sculpture">
-          <div className="border-2 border-white p-7 text-white text-xl tracking-widest uppercase bg-clip-border bg-transparent bg-origin-padding">
-            Gabriella Furno
+          <div className="border-2 border-black p-7 text-black text-xl tracking-widest uppercase bg-clip-border bg-transparent bg-origin-padding">
+            From Audemars to Zenith
           </div>
         </Link>
       </div>
 
       <div className="relative flex justify-center">
-        <Link href="/sculpture">
+        <Link href="/">
           <Image
             priority
             alt=''
+            src={homePage1}
             style={{
               width: '100%',
               height: '100vh',
